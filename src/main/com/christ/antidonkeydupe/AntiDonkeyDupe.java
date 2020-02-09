1192261fd15c8b376e7b6544e0415d6d18030e7c
pineapplebox <37129442+pineapplebox@users.noreply.github.com>
2020-02-08 17:43:55 -0800

    Create AntiDonkeyDupe.java

---
+++ src/main/com/christ/antidonkeydupe/AntiDonkeyDupe.java
@@ -0,0 +1,37 @@
+public class AntiDupe extends JavaPlugin {
+    
+    @Override
+	public void onLoad() {
+		plugin = this;
+
+		getLogger().info("[AntiDonkeyDupe] Successfully Enabled");
+	}
+    
+    @Override
+    public void onEnable() {
+        ProtocolLibrary.getProtocolManager().addPacketListener(
+            new PacketAdapter(this, PacketType.CPacketInput) {
+            @Override
+              
+                public void onTime(final Player p) {
+                    Bukkit.getScheduler().runTaskLater(Core.getInstance(), new Runnable() {
+                    @Override
+                    public void run() {
+                        if(p.isInsideVehicle()) {
+                            if (p.getVehicle() != null) {
+                                public void onPacketReceiving(PacketEvent event) {
+
+                                    if (event.getPacketType() == PacketType.CPacketInput) {
+                                        event.setCancelled(true);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    },20L);
+                }
+            
+            });
+    }
+    
+}
