564863a546a8e79529d6f5b609cdafd36048e45a
pineapplebox <37129442+pineapplebox@users.noreply.github.com>
2020-02-08 19:33:08 -0800

    Update AntiDonkeyDupe.java

--- src/main/java/com/christ/AntiDonkeyDupe/AntiDonkeyDupe.java
+++ src/main/java/com/christ/AntiDonkeyDupe/AntiDonkeyDupe.java
@@ -1,4 +1,10 @@
-public class AntiDupe extends JavaPlugin {
+package com.christ.AntiDonkeyDupe;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AntiDonkeyDupe extends JavaPlugin {
     
     @Override
     public void onLoad() {
