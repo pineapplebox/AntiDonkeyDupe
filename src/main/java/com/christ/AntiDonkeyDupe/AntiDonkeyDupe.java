7d158ee71cbdc559b1de43951b6838ab08cf95ad
pineapplebox <37129442+pineapplebox@users.noreply.github.com>
2020-02-09 08:20:02 -0800

    Update AntiDonkeyDupe.java

--- src/main/java/com/christ/AntiDonkeyDupe/AntiDonkeyDupe.java
+++ src/main/java/com/christ/AntiDonkeyDupe/AntiDonkeyDupe.java
@@ -15,28 +15,29 @@ public class AntiDonkeyDupe extends JavaPlugin implements Listener {
 	}
 	
     @Override
-    	public void onEnable() {
-        	ProtocolLibrary.getProtocolManager().addPacketListener(
-		new PacketAdapter(this, PacketType.Play.Client.CPacketInput) {
-		});
+    public void onEnable() {
+	protocolManager = ProtocolLibrary.getProtocolManager();
+    }
+              
+    protocolManager.addPacketListener(
+	new PacketAdapter(this, ListenerPriority.NORMAL, 
+        PacketType.Play.Client.CPacketInput) {
+    	    public void onTime(final Player p) {
+		Bukkit.getScheduler().runTaskLater(Core.getInstance(), new Runnable() {
+	    	    public void run() {
+			if(p.isInsideVehicle()) {
+		    	    if (p.getVehicle() != null) {
+				break run;
+				public void onPacketReceiving(PacketEvent e) {
+			            if (e.getPacketType() == PacketType.CPacketInput) {
+				        e.setCancelled(true);
+			    	    }
+				}
+		    	    }
+			}
+	    	    }
+		},20L);
+	    }
 	}
-	
-	public void onPacketReceiving(PacketEvent e) {
-		if (e.getPacketType() == PacketType.Play.Client.CPacketInput) {
-			e.setCancelled(true);
-		}
-	}
-	
-	public void onTime(final Player p) {
-        	Bukkit.getScheduler().runTaskLater(Core.getInstance(), new Runnable() {
-                	@Override
-                	public void run() {
-                        	if(p.isInsideVehicle()) {
-                            		if (p.getVehicle() != null) {
-						onPacketReceiving();
-                            		}
-                        	}
-                    	}
-                },20L);
-        }
+    });
 }
