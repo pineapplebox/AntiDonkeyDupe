edcd83d15669652daa92cb4659f5eab9803af17f
pineapplebox <37129442+pineapplebox@users.noreply.github.com>
2020-03-30 11:38:09 -0400

    Update AntiDonkeyDupe.java

--- src/main/java/com/christ/AntiDonkeyDupe/AntiDonkeyDupe.java
+++ src/main/java/com/christ/AntiDonkeyDupe/AntiDonkeyDupe.java
@@ -5,18 +5,18 @@ import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
 import com.comphenix.protocol.events.PacketAdapter;
 
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+
+import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
-
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
+import org.bukkit.entity;
 
 public class AntiDonkeyDupe extends JavaPlugin {
     @Override
     public void onEnable() {
-        plugin = this;
         getLogger().info("[AntiDonkeyDupe] Successfully Enabled");
     }
     public void onDisable() {
@@ -24,8 +24,7 @@ public class AntiDonkeyDupe extends JavaPlugin {
     }
     private void protocolPacketListener() {
         ProtocolLibrary.getProtocolManager().addPacketListener(
-          (PacketListener)new PacketAdapter(this, new PacketType[] { PacketType.Play.Client.CPacketInput }) {
-            public void onPacketReceiving(final PacketEvent event) {
+          (PacketListener)new PacketAdapter(this, new PacketType[] { PacketType.Play.Client.CPacketInput }) { public void onPacketReceiving(final PacketEvent event) {
                 if (event.getPacketType().equals((Object)PacketType.Play.Client.CPacketInput)) {
                     Player p = event.getPlayer();
                     if(p.isInsideVehicle()) {
