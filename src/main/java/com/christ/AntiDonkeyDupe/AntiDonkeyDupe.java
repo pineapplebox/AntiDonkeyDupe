cc1b20e53b5d1b94ed9b68e5aea4cf90ddb74361
pineapplebox <37129442+pineapplebox@users.noreply.github.com>
2020-03-30 09:44:44 -0400

    Update AntiDonkeyDupe.java

--- src/main/java/com/christ/AntiDonkeyDupe/AntiDonkeyDupe.java
+++ src/main/java/com/christ/AntiDonkeyDupe/AntiDonkeyDupe.java
@@ -5,7 +5,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class AntiDonkeyDupe extends JavaPlugin implements Listener {
+public class AntiDupe extends JavaPlugin {
     
     @Override
 	public void onLoad() {
@@ -13,33 +13,24 @@ public class AntiDonkeyDupe extends JavaPlugin implements Listener {
 
 		getLogger().info("[AntiDonkeyDupe] Successfully Enabled");
 	}
-
+    
     @Override
     public void onEnable() {
-        protocolManager = ProtocolLibrary.getProtocolManager();
-    }
-
-    public void onTime(final Player p) {
-        Bukkit.getScheduler().runTaskLater(Core.getInstance(), new Runnable() {
+        ProtocolLibrary.getProtocolManager().addPacketListener(
+            new PacketAdapter(this, PacketType.CPacketInput) {
             @Override
-            public void run() {
-                if(p.isInsideVehicle()) {
-                    if (p.getVehicle() != null) {
-                        protocolManager.addPacketListener(
-                            new PacketAdapter(this, ListenerPriority.NORMAL, 
-                            PacketType.Play.Client.CPacketInput) {
-                            @Override
-                                private void onPacketReceiving(PacketEvent event) {
-                                    if (event.getPacketType() == 
-                                    PacketType.Play.Client.CPacketInput) {
-                                        event.setCancelled(true);
-                                    }
+                public void onPacketReceiving(PacketEvent event) {
+                    Player p = event.getPlayer();
+                        Bukkit.getScheduler().runTaskLater(Core.getInstance(), new Runnable() {
+                        @Override
+                            if(p.isInsideVehicle()) {
+                                if (event.getPacketType() == PacketType.CPacketInput) {
+                                    event.setCancelled(true);
+                                }
                             }
-                            break onPacketReceiving;
-                        });
+                        },20L);
                     }
                 }
-            }
-        },20L);
+            });
     }
 }
