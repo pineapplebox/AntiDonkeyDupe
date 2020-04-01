1723f7f19609d9cdff4919a649a0a97e89972949
pineapplebox <37129442+pineapplebox@users.noreply.github.com>
2020-04-01 10:21:42 -0400

    Update AntiDonkeyDupe.java

--- src/main/java/com/christ/AntiDonkeyDupe/AntiDonkeyDupe.java
+++ src/main/java/com/christ/AntiDonkeyDupe/AntiDonkeyDupe.java
@@ -1,38 +1,66 @@
 package com.christ.AntiDonkeyDupe;
 
-import com.comphenix.protocol.events.PacketListener;
-import com.comphenix.protocol.events.PacketContainer;
-import com.comphenix.protocol.events.PacketEvent;
-import com.comphenix.protocol.events.PacketAdapter;
-
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
-
-import org.bukkit.plugin.java.JavaPlugin;
+import io.netty.channel.*;
+import net.minecraft.server.v1_12_R1.PacketPlayInSteerVehicle
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.entity.Player;
-import org.bukkit.entity;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 
-public class AntiDonkeyDupe extends JavaPlugin {
+public class AntiDonkeyDupe extends JavaPlugin implements Listener {
+
     @Override
     public void onEnable() {
-        getLogger().info("[AntiDonkeyDupe] Successfully Enabled");
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
-    public void onDisable() {
-        ProtocolLibrary.getProtocolManager().removePacketListeners((Plugin)this);
+
+    @EventHandler
+    public void onjoin(PlayerJoinEvent event){
+        injectPlayer(event.getPlayer());
     }
-    private void protocolPacketListener() {
-        ProtocolLibrary.getProtocolManager().addPacketListener(
-          (PacketListener)new PacketAdapter(this, new PacketType[] { PacketType.Play.Client.CPacketInput }) { public void onPacketReceiving(final PacketEvent event) {
-                if (event.getPacketType().equals((Object)PacketType.Play.Client.CPacketInput)) {
-                    Player p = event.getPlayer();
-                    if(p.isInsideVehicle()) {
-                        event.setCancelled(true);
+
+    @EventHandler
+    public void onleave(PlayerQuitEvent event){
+        removePlayer(event.getPlayer());
+    }
+    private void removePlayer(Player player) {
+        Channel channel = ((CraftPlayer) player).getHandle().playerConnection.networkManager.channel;
+        channel.eventLoop().submit(() -> {
+            channel.pipeline().remove(player.getName());
+            return null;
+        });
+    }
+
+    private void injectPlayer(Player player) {
+        ChannelDuplexHandler channelDuplexHandler = new ChannelDuplexHandler() {
+
+            @Override
+            public void channelRead(ChannelHandlerContext channelHandlerContext, Object packet) throws Exception {
+                
+                if(player.isInsideVehicle()) {
+                    if(packet instanceof PacketPlayInSteerVehicle){
+                        PacketPlayInSteerVehicle PacketPlayInSteerVehicle = (PacketPlayInSteerVehicle) packet;
                         return;
                     }
                 }
+                super.channelRead(channelHandlerContext, packet);
             }
-        });
+
+            @Override
+            public void write(ChannelHandlerContext channelHandlerContext, Object packet, ChannelPromise channelPromise) throws Exception {
+                super.write(channelHandlerContext, packet, channelPromise);
+            }
+
+
+        };
+
+        ChannelPipeline pipeline = ((CraftPlayer) player).getHandle().playerConnection.networkManager.channel.pipeline();
+        pipeline.addBefore("packet_handler", player.getName(), channelDuplexHandler);
+
     }
 }
