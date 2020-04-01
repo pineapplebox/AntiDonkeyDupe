566da7d6ffea5bcbe4887b01b05e03ca3f863fb9
pineapplebox <37129442+pineapplebox@users.noreply.github.com>
2020-04-01 19:15:54 -0400

    Update AntiDonkeyDupe.java

--- src/main/java/com/christ/AntiDonkeyDupe/AntiDonkeyDupe.java
+++ src/main/java/com/christ/AntiDonkeyDupe/AntiDonkeyDupe.java
@@ -56,15 +56,14 @@ public class AntiDonkeyDupe extends JavaPlugin {
             }
         });
     }
-    private void setupPortalPatch() {
-        @EventHandler(priority = EventPriority.LOWEST)
-        public void onPortalCreate(final PortalCreateEvent e) {
-            ArrayList<BlockState> b = (ArrayList<BlockState>) e.getBlocks();
-            World w = e.getWorld();
-            int height = b.get(0).getLocation().getBlockY();
-            if (height >= 128 && w.getName().endsWith("_nether")) {
-                    e.setCancelled(true);
-            }
+    
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPortalCreate(final PortalCreateEvent e) {
+        ArrayList<BlockState> b = (ArrayList<BlockState>) e.getBlocks();
+        World w = e.getWorld();
+        int height = b.get(0).getLocation().getBlockY();
+        if (height >= 128 && w.getName().endsWith("_nether")) {
+                e.setCancelled(true);
         }
     }
 }
