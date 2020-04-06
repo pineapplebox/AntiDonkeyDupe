90b21a261a58675858427ac88df2717f4a389105
pineapplebox <37129442+pineapplebox@users.noreply.github.com>
2020-04-05 21:22:43 -0400

    Update AntiDonkeyDupe.java

--- src/main/java/com/christ/AntiDonkeyDupe/AntiDonkeyDupe.java
+++ src/main/java/com/christ/AntiDonkeyDupe/AntiDonkeyDupe.java
@@ -10,11 +10,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.world.PortalCreateEvent;
 import org.bukkit.entity.Player;
 import org.bukkit.Location;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.entity.EntityType;
+
+import net.minecraft.server.v1_12_R1.PacketPlayInSteerVehicle;
 
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
@@ -26,31 +31,32 @@ public class AntiDonkeyDupe extends JavaPlugin implements Listener {
         System.out.println("[AntiDonkeyDupe] Plugin successfully enabled.");
     }
     public void onDisable() {
+        System.out.println("[AntiDonkeyDupe] Plugin successfully disabled.");
         ProtocolLibrary.getProtocolManager().removePacketListeners((Plugin)this);
     }
     private void useProtocolLib() {
         ProtocolLibrary.getProtocolManager().addPacketListener((PacketListener)new PacketAdapter(this, new PacketType[] { PacketType.Play.Client.STEER_VEHICLE }) {
             public void onPacketReceiving(final PacketEvent event) {
-                final PacketType packetType = event.getPacketType();
-                if (packetType.equals((Object)PacketType.Play.Client.STEER_VEHICLE)) {
+                if(event.getPacketType().equals(PacketType.Play.Client.STEER_VEHICLE)){
                     Player p = event.getPlayer();
-                    if(p.isInsideVehicle()) {
-                        event.setCancelled(true);
-                        return;
+                    Entity vehicle = p.getVehicle();
+                    PacketPlayInSteerVehicle ppisv = (PacketPlayInSteerVehicle) event.getPacket().getHandle();
+                    boolean shift = ppisv.d();
+                    event.setCancelled(true);
+                    if (shift == true){
+                        Bukkit.getScheduler().runTaskLater(plugin, () -> {
+                            vehicle.eject();
+                            Bukkit.getScheduler().runTaskLater(plugin, () -> {
+                                if(p.isSneaking()) {
+                                    return;
+                                } else {
+                                    vehicle.addPassenger(p);
+                                }
+                            }, 3L);
+                        }, 2L);
                     }
                 }
             }
         });
-    }
-    @EventHandler
-    public void onVehicleExit(VehicleExitEvent event) {
-        Player player = event.getPlayer();
-        Boolean isSneaking = player.isSneaking();
-        if(isSneaking) {
-            if(player.isInsideVehicle()) {
-	        player.eject();
-	        return;
-	    }
-        }
     }
 }
